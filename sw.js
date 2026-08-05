be3085a20186ecc58c769fce37900b52c75440e1
bj21z <bj21z@sina.com>
2026-08-05 19:54:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yunshang-xiaobaihua-v1.1.0';
+const CACHE='yunshang-xiaobaihua-1.2.0';
 const ASSETS=['./','./index.html','./styles.css?v=1.1.0','./app.js?v=1.1.0','./manifest.webmanifest','./assets/icon.svg','./assets/images/longmen.jpg','./data/dynamics.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
